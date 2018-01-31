9b8dff6c58182e9e30d98261c8cc2efde3afd51b
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2018-01-31 15:34:03 +0100

    adjust migrations scripts for mysql

--- src/PH/PaymentHubBundle/Migrations/Schema/v1_15/PaymentHubBundleMigration.php
+++ src/PH/PaymentHubBundle/Migrations/Schema/v1_15/PaymentHubBundleMigration.php
@@ -13,39 +13,39 @@ class PaymentHubBundleMigration implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN orderid TO order_id;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN providertype TO provider_type;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN purchasestate TO purchase_state;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN paymentstate TO payment_state;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN checkoutcompletedat TO checkout_completed_at;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN updatedat TO updated_at;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN activationemailsend TO activation_email_send;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN enddate TO end_date;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN createdat TO created_at;');
-        $queries->addQuery('ALTER TABLE ph_subscription RENAME COLUMN startdate TO start_date;');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE orderid order_id VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE providertype provider_type VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE purchasestate purchase_state VARCHAR(255) NOT NULL;');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE paymentstate payment_state VARCHAR(255) NOT NULL;');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE checkoutcompletedat checkout_completed_at DATETIME COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE updatedat updated_at DATETIME COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE activationemailsend activation_email_send DATETIME COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE enddate end_date DATETIME COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE createdat created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_subscription CHANGE startdate start_date DATE COMMENT \'(DC2Type:date)\';');
 
-        $queries->addQuery('ALTER TABLE ph_notification_log RENAME COLUMN emailcontent TO email_content;');
-        $queries->addQuery('ALTER TABLE ph_notification_log RENAME COLUMN sendat TO send_at;');
+        $queries->addQuery('ALTER TABLE ph_notification_log CHANGE emailcontent email_content LONGTEXT;');
+        $queries->addQuery('ALTER TABLE ph_notification_log CHANGE sendAt send_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\';');
 
-        $queries->addQuery('ALTER TABLE ph_order_item RENAME COLUMN orderitemid TO order_item_id;');
-        $queries->addQuery('ALTER TABLE ph_order_item RENAME COLUMN unitprice TO unit_price;');
-        $queries->addQuery('ALTER TABLE ph_order_item RENAME COLUMN createdat TO created_at;');
-        $queries->addQuery('ALTER TABLE ph_order_item RENAME COLUMN updatedat TO updated_at;');
+        $queries->addQuery('ALTER TABLE ph_order_item CHANGE orderitemid order_item_id VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_order_item CHANGE unitprice unit_price DOUBLE NOT NULL;');
+        $queries->addQuery('ALTER TABLE ph_order_item CHANGE createdat created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_order_item CHANGE updatedat updated_at DATETIME COMMENT \'(DC2Type:datetime)\';');
 
-        $queries->addQuery('ALTER TABLE ph_payment RENAME COLUMN paymentid TO payment_id;');
-        $queries->addQuery('ALTER TABLE ph_payment RENAME COLUMN methodcode TO method_code;');
-        $queries->addQuery('ALTER TABLE ph_payment RENAME COLUMN holdername TO holder_name;');
-        $queries->addQuery('ALTER TABLE ph_payment RENAME COLUMN currencycode TO currency_code;');
-        $queries->addQuery('ALTER TABLE ph_payment RENAME COLUMN createdat TO created_at;');
-        $queries->addQuery('ALTER TABLE ph_payment RENAME COLUMN updatedat TO updated_at;');
+        $queries->addQuery('ALTER TABLE ph_payment CHANGE paymentid payment_id VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_payment CHANGE methodcode method_code VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_payment CHANGE holdername holder_name VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_payment CHANGE currencycode currency_code VARCHAR(255) NOT NULL;');
+        $queries->addQuery('ALTER TABLE ph_payment CHANGE createdat created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\';');
+        $queries->addQuery('ALTER TABLE ph_payment CHANGE updatedat updated_at DATETIME COMMENT \'(DC2Type:datetime)\';');
 
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN newsletterallowed TO newsletter_allowed;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN giftallowed TO gift_allowed;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN publiccomment TO public_comment;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN customerupdatetoken TO customer_update_token;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN contactforbidden TO contact_forbidden;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN emailverificationtoken TO email_verification_token;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN processemailverification TO process_email_verification;');
-        $queries->addQuery('ALTER TABLE ph_customer RENAME COLUMN emailverifiedat TO email_verified_at;');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE newsletterallowed newsletter_allowed TINYINT(1);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE giftallowed gift_allowed TINYINT(1);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE publiccomment public_comment TINYINT(1);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE customerupdatetoken customer_update_token VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE contactforbidden contact_forbidden TINYINT(1);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE emailverificationtoken email_verification_token VARCHAR(255);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE processemailverification process_email_verification TINYINT(1);');
+        $queries->addQuery('ALTER TABLE ph_customer CHANGE emailVerifiedAt email_verified_at DATETIME COMMENT \'(DC2Type:datetime)\';');
     }
 }
